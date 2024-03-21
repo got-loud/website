8f816148a97f0b5709b59e403648d0e17366d72a
Kirk Davies <davi0534@algonquinlive.com>
2024-03-20 21:03:57 -0400

    Beginning to implement got-loud

---
+++ src/app/components/portfolio/models/portfolio-item-options.interface.ts
@@ -0,0 +1,6 @@
+export interface PortfolioItemOptionsInterface {
+  imageUrl: string;
+  imageAlt: string;
+  title: string;
+  description: string;
+}
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,12 +1,107 @@
 import { Component } from '@angular/core';
+import {PortfolioItemComponent} from "./components/portfolio-item/portfolio-item.component";
+import {PortfolioItemOptionsInterface} from "./models/portfolio-item-options.interface";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [
+    PortfolioItemComponent,
+    NgForOf
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+  items: PortfolioItemOptionsInterface[] = [
+    {
+      imageUrl: this.assetUrl('a.jpg'),
+      imageAlt: 'Blunt Fiction Event',
+      title: 'Blunt Fiction Event',
+      description: 'Pulp Fiction Themed Event'
+    },
+    {
+      imageUrl: this.assetUrl('b.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('c.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('d.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('e.jpg'),
+      imageAlt: 'Blunt Fiction Event',
+      title: 'Blunt Fiction Event',
+      description: 'Pulp Fiction Themed Event'
+    },
+    {
+      imageUrl: this.assetUrl('f.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('g.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('h.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('i.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('j.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('k.jpg'),
+      imageAlt: 'Blunt Fiction Event',
+      title: 'Blunt Fiction Event',
+      description: 'Pulp Fiction Themed Event'
+    },
+    {
+      imageUrl: this.assetUrl('l.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('m.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+    {
+      imageUrl: this.assetUrl('n.jpg'),
+      imageAlt: 'Loud Mouth Event',
+      title: 'Loud Mouth Event',
+      description: 'Get Loud & Bare Jokes Presents'
+    },
+  ]
 
+  private assetUrl(nameAndExtension: string) {
+    return `/assets/images/portfolio/${nameAndExtension}`;
+  }
 }
